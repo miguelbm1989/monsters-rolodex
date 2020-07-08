b5c72d8914b27c67b49f8905fcac5767f5c4f502
miguelbm1989 <miguelbm1989@gmail.com>
2020-07-08 20:18:02 +0200

    Updates

---
+++ precache-manifest.e155048a29ef4fc6f8ed5666891917ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a00acf88476becbcc639d63b0b911c5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "333bf2befb9aa4dbdd2b",
+    "url": "/monsters-rolodex/static/css/main.51e0706a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "333bf2befb9aa4dbdd2b",
+    "url": "/monsters-rolodex/static/js/main.3b2af1ba.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
